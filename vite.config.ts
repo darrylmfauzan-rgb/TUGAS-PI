53417ac9c349cdda52033bbb2475f8cab5480fe7
darrylmfauzan-rgb <darrylmfauzan@gmail.com>
2026-07-15 21:28:47 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,17 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  root: path.resolve(__dirname),
+  build: {
+    outDir: "dist",
+  },
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean,
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
